a737ba37892647e6dcb1aff4e3f57fa188a35059
GreenLungus <104139639+GreenLungus@users.noreply.github.com>
2023-07-01 19:53:16 +0200

    implemented loop to create cards out of parsed json

--- app/src/main/java/com/example/mobappfrontend/MainActivity.kt
+++ app/src/main/java/com/example/mobappfrontend/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.mobappfrontend
 
 import android.content.Context
 import android.content.Intent
-//import android.media.Image
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -22,29 +21,25 @@ import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-//import androidx.core.content.ContextCompat.startActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.LocationOn
-//import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-//import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
-//import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import coil.compose.rememberAsyncImagePainter
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 
-data class JsonObject(val id: Int, val data: String)
-
+//-----------------COLORS-----------------<<<<<<<<
 //for dropdown spinner background
 val customColor1 = Color.hsl(228F, 0.6F,0.6F,1f, ColorSpaces.Srgb)
 //for middle section background
@@ -55,14 +50,59 @@ val cities = listOf(
     "Hannover", "Düsseldorf", "Mainz", "Saarbrücken", "Dresden", "Magdeburg", "Kiel", "Erfurt"
 )
 
-val jsonData = mutableStateListOf(
-    JsonObject(1, "Data 1"),
-    JsonObject(2, "Data 2"),
-    JsonObject(3, "Data 3"),
-    // ... add more JSON objects as needed
+var showContainer: ShowContainer? = null;
+
+//-----------------LazyColumn Dataclass-----------------<<<<<<<<
+data class Topiccard(
+    val title: String,
+    val url: String,
+    val img: String
 )
 
-var showContainer: ShowContainer? = null;
+//liste aus Json datensatz generieren und an datenklasse übergeben, diese datenklassen als liste an lazycolumn übergeben
+val topicCardsList :MutableList<Topiccard> = mutableListOf(
+    /*
+        Topiccard("So sicher wie Fort Knox (128)", "https://www.ardmediathek.de/video/ODFlZmExYzUtZWJlMy00YTA2LWFlOTQtNTU3MTg1ZGRiODVk", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:1349ca4a85ad8334/16x9?imwidth=1920&w=1920"),
+        Topiccard("Tatort: Hinter dem Spiegel", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE4MDA0MzU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a29af08618b987d8/16x9?imwidth=1920&w=1920"),
+        Topiccard("Das letzte Rennen", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE3NDYzODU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a8ce3a563082adae/16x9?imwidth=1920&w=1920"),
+        Topiccard("So sicher wie Fort Knox (128)", "https://www.ardmediathek.de/video/ODFlZmExYzUtZWJlMy00YTA2LWFlOTQtNTU3MTg1ZGRiODVk", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:1349ca4a85ad8334/16x9?imwidth=1920&w=1920"),
+        Topiccard("Tatort: Hinter dem Spiegel", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE4MDA0MzU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a29af08618b987d8/16x9?imwidth=1920&w=1920"),
+        Topiccard("Das letzte Rennen", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE3NDYzODU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a8ce3a563082adae/16x9?imwidth=1920&w=1920"),
+        Topiccard("So sicher wie Fort Knox (128)", "https://www.ardmediathek.de/video/ODFlZmExYzUtZWJlMy00YTA2LWFlOTQtNTU3MTg1ZGRiODVk", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:1349ca4a85ad8334/16x9?imwidth=1920&w=1920"),
+        Topiccard("Tatort: Hinter dem Spiegel", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE4MDA0MzU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a29af08618b987d8/16x9?imwidth=1920&w=1920"),
+        Topiccard("Das letzte Rennen", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE3NDYzODU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a8ce3a563082adae/16x9?imwidth=1920&w=1920"),
+        Topiccard("So sicher wie Fort Knox (128)", "https://www.ardmediathek.de/video/ODFlZmExYzUtZWJlMy00YTA2LWFlOTQtNTU3MTg1ZGRiODVk", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:1349ca4a85ad8334/16x9?imwidth=1920&w=1920"),
+        Topiccard("Tatort: Hinter dem Spiegel", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE4MDA0MzU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a29af08618b987d8/16x9?imwidth=1920&w=1920"),
+        Topiccard("Das letzte Rennen", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE3NDYzODU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a8ce3a563082adae/16x9?imwidth=1920&w=1920")
+    */
+)
+
+//-----------------Parser Dataclasses-----------------<<<<<<<<
+@Serializable
+data class Image(
+    val url: String
+)
+
+@Serializable
+data class Show(
+    val title: String,
+    val links: Links,
+    val images: List<Image>,
+    val keywords: List<String>
+)
+
+@Serializable
+data class Links(
+    val web: String
+)
+
+@Serializable
+data class ShowContainer(
+    val items: List<Show>
+)
+
+
+
 //------------->>>------------->>> Main
 class MainActivity : ComponentActivity() { //ComponentActivity() or AppCompatActivity() ??
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,9 +114,12 @@ class MainActivity : ComponentActivity() { //ComponentActivity() or AppCompatAct
                 for (show in showContainer!!.items) {
                     println("Title: ${show.title}")
                     println("Web link: ${show.links.web}")
+
                     for (image in show.images) {
-                            println("Image URL: ${image.url}")
-                        }
+                        println("Image URL: ${image.url}")
+                        topicCardsList.add(Topiccard(show.title, show.links.web, image.url))
+
+                    }
                 }
             }
 
@@ -276,14 +319,11 @@ fun TopicCards(topic: Topiccard) {
             modifier = Modifier
                 .padding(5.dp)
         ) {
-            //Image preview
-            //Text(text = "IMAGE\n  here", color = Color.White, fontSize = 15.sp)
-
             val image: Painter = rememberAsyncImagePainter(topic.img)
             Image(
                 modifier = Modifier
-                    .size(80.dp, 80.dp)
-                    .clip(RoundedCornerShape(16.dp)),
+                    .size(95.dp, 100.dp)
+                    .clip(RoundedCornerShape(10.dp)),
                 painter = image,
                 alignment = Alignment.CenterStart,
                 contentDescription = "",
@@ -293,17 +333,17 @@ fun TopicCards(topic: Topiccard) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .padding(5.dp)
+                    //.padding(5.dp)
                     .weight(2f)
             ) {
-                //var link = topic.url
-                //Text(text = "This is Title ***************", color = Color.White, fontSize = 20.sp)
 
                 Text(
                     text = topic.title,
+                    textAlign = TextAlign.Center,
                     fontWeight = FontWeight.Bold,
                     color = Color.White,
                     fontSize = 17.sp
+
                 )
                 OpenLinkButton(link = topic.url)
             }
@@ -324,36 +364,8 @@ fun beRequestMan(dbitem: String) {
 //2. count rows? words? to determine where the index is and an object starts
 //3. deserialize using gson and write into datac lass and add data class object to list
 //4. use data class list to fill list on main screen
-//TODO: convert Json object to kotlin data class
-data class Topiccard(
-    val title: String,
-    val url: String,
-    val img: String
-)
-@Serializable
-data class Image(
-    val url: String
-)
-
-@Serializable
-data class Show(
-    val title: String,
-    val links: Links,
-    val images: List<Image>,
-    val keywords: List<String>
-)
-
-@Serializable
-data class Links(
-    val web: String
-)
-
-@Serializable
-data class ShowContainer(
-    val items: List<Show>
-)
-
-// Parse through the json file and safe shows in global
+/*TODO: convert Json object to kotlin data class*/
+//Parse through the json file and safe shows in global
 fun parse(context: Context) {
     val inputStream = context.resources.openRawResource(R.raw.filtered_shows)
     val jsonString = inputStream.bufferedReader().use { it.readText() }
@@ -366,21 +378,6 @@ fun parse(context: Context) {
 @Composable
 fun LazyColumnTopics() {
     LazyColumn {
-        //liste aus Json datensatz generieren und an datenklasse übergeben, diese datenklassen als liste an lazycolumn übergeben
-        val topicCardsList :List<Topiccard> = listOf(
-            Topiccard("So sicher wie Fort Knox (128)", "https://www.ardmediathek.de/video/ODFlZmExYzUtZWJlMy00YTA2LWFlOTQtNTU3MTg1ZGRiODVk", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:1349ca4a85ad8334/16x9?imwidth=1920&w=1920"),
-            Topiccard("Tatort: Hinter dem Spiegel", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE4MDA0MzU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a29af08618b987d8/16x9?imwidth=1920&w=1920"),
-            Topiccard("Das letzte Rennen", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE3NDYzODU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a8ce3a563082adae/16x9?imwidth=1920&w=1920"),
-            Topiccard("So sicher wie Fort Knox (128)", "https://www.ardmediathek.de/video/ODFlZmExYzUtZWJlMy00YTA2LWFlOTQtNTU3MTg1ZGRiODVk", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:1349ca4a85ad8334/16x9?imwidth=1920&w=1920"),
-            Topiccard("Tatort: Hinter dem Spiegel", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE4MDA0MzU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a29af08618b987d8/16x9?imwidth=1920&w=1920"),
-            Topiccard("Das letzte Rennen", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE3NDYzODU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a8ce3a563082adae/16x9?imwidth=1920&w=1920"),
-            Topiccard("So sicher wie Fort Knox (128)", "https://www.ardmediathek.de/video/ODFlZmExYzUtZWJlMy00YTA2LWFlOTQtNTU3MTg1ZGRiODVk", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:1349ca4a85ad8334/16x9?imwidth=1920&w=1920"),
-            Topiccard("Tatort: Hinter dem Spiegel", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE4MDA0MzU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a29af08618b987d8/16x9?imwidth=1920&w=1920"),
-            Topiccard("Das letzte Rennen", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE3NDYzODU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a8ce3a563082adae/16x9?imwidth=1920&w=1920"),
-            Topiccard("So sicher wie Fort Knox (128)", "https://www.ardmediathek.de/video/ODFlZmExYzUtZWJlMy00YTA2LWFlOTQtNTU3MTg1ZGRiODVk", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:1349ca4a85ad8334/16x9?imwidth=1920&w=1920"),
-            Topiccard("Tatort: Hinter dem Spiegel", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE4MDA0MzU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a29af08618b987d8/16x9?imwidth=1920&w=1920"),
-            Topiccard("Das letzte Rennen", "https://www.ardmediathek.de/video/Y3JpZDovL3N3ci5kZS9hZXgvbzE3NDYzODU", "https://api.ardmediathek.de/image-service/image-collections/urn:ard:image-collection:a8ce3a563082adae/16x9?imwidth=1920&w=1920")
-        )
         items(topicCardsList) {
             TopicCards(topic = it)
         }
