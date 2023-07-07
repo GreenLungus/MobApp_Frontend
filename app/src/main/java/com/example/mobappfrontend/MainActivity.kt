a0c249d8ed4cc07e7da2ec3f07959ab030758537
GreenLungus <104139639+GreenLungus@users.noreply.github.com>
2023-07-07 18:39:17 +0200

    .

--- app/src/main/java/com/example/mobappfrontend/MainActivity.kt
+++ app/src/main/java/com/example/mobappfrontend/MainActivity.kt
@@ -11,7 +11,7 @@ import com.google.android.gms.location.LocationCallback
 
 
 
-//-----------------COLORS-----------------<<<<<<<<
+//-----------------fast access custom COLORS-----------------<<<<<<<<
 //for dropdown spinner background
 val customColor1 = Color.hsl(228F, 0.6F,0.6F,1f, ColorSpaces.Srgb)
 //for middle section background
@@ -19,11 +19,13 @@ val customColor2 = Color.hsl(226F, 0.4F,0.7F,1f, ColorSpaces.Srgb)
 //for ARD logo
 val customColor3 = Color.hsl(180F, 0.6F,0.7F,1f, ColorSpaces.Srgb)
 
+
 //-----------------Dropdown selectables-----------------<<<<<<<<
 val cities = listOf(
     "Stuttgart", "München", "Berlin", "Potsdam", "Bremen", "Hamburg", "Wiesbaden", "Schwerin",
     "Hannover", "Düsseldorf", "Mainz", "Saarbrücken", "Dresden", "Magdeburg", "Kiel", "Erfurt"
 )
+
 
 //-----------------List of data objects for Topiccards-----------------<<<<<<<<
 var topicCardsList :MutableList<Topiccard> = mutableListOf()
@@ -32,10 +34,12 @@ var topicCardsList :MutableList<Topiccard> = mutableListOf()
 //-----------------Container: Data Objects generated out of Json file-----------------<<<<<<<<
 var showContainer: ShowContainer? = null
 
+
 //-----------------Location-----------------<<<<<<<<
 var locationCallback: LocationCallback? = null
 var fusedLocationClient: FusedLocationProviderClient? = null
 var locationRequired = false
+
 
 // array of all needed permissions to check with
 val permissions = arrayOf(
@@ -47,15 +51,13 @@ var currentLocationForPrint = LocationDetails(0.toDouble(), 0.toDouble())
 
 
 //------------->>>------------->>> Main
-class MainActivity : ComponentActivity() { //ComponentActivity() or AppCompatActivity() ??
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //val context = LocalContext.current
 
             Locationing(this)
             AppContent(this)
-
 
         }
     }
