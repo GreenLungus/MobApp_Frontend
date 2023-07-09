564c5e99505f29ef39610f75c25581d54ee2112e
JulianDanzer <juliandanzer@outlook.com>
2023-07-09 10:43:16 +0200

    Second request for backend
    created location request

--- app/src/main/java/com/example/mobappfrontend/Connections.kt
+++ app/src/main/java/com/example/mobappfrontend/Connections.kt
@@ -28,61 +28,53 @@ suspend fun beRequestMan(context: Context, dbitem: String) {
 
     // Sending GET-Request to Backend-Server
     val url = "http://10.0.2.2:8080/$dbitem"
-    val response: String = client.get(url) //TODO: catch crashing with try and catch
+    try {
+        val response: String = client.get(url)
 
-    // Writes response to json file and fills dataclass
-    context.openFileOutput("filtered_shows.json", Context.MODE_PRIVATE).use {
-        it.write(response.toByteArray())
-    }
-    parse(context)
-
-    // close HTTP-Client
-    client.close()
-}
-
-//Try catch block didnt resolve crashing problem...
-/*
-suspend fun beRequestMan(context: Context, dbitem: String): List<ClipData.Item>{
-
-        try {
-            // Creation of HTTP-Client
-            val client = HttpClient {
-                install(JsonFeature) {
-                    serializer = KotlinxSerializer(Json {
-                        prettyPrint = true
-                        isLenient = true
-                        ignoreUnknownKeys = true
-                    })
-                }
-            }
-            // Sending GET-Request to Backend-Server
-            val url = "http://10.0.2.2:8080/$dbitem"
-            lateinit var response: String //TODO: catch crashing with try and catch
-            //val response: String = client.get(url)
-            response = client.get(url)
-            // Writes response to json file and fills dataclass
-            context.openFileOutput("filtered_shows.json", Context.MODE_PRIVATE).use {
-                it.write(response.toByteArray())
-            }
-            parse(context)
-
-            // close HTTP-Client
-            client.close()
-            return emptyList()
-
-
-        } catch (e: RedirectResponseException){
-            Toast.makeText(context, "Server Fehler", Toast.LENGTH_LONG).show()
-            return emptyList()
-        } catch (e: ClientRequestException){
-            Toast.makeText(context, "Server Fehler", Toast.LENGTH_LONG).show()
-            return emptyList()
-        } catch (e: ServerResponseException){
-            Toast.makeText(context, "Server Fehler", Toast.LENGTH_LONG).show()
-            return emptyList()
-        } catch (e: Exception){
-            Toast.makeText(context, "Server Fehler", Toast.LENGTH_LONG).show()
-            return emptyList()
+        // Writes response to json file and fills dataclass
+        context.openFileOutput("filtered_shows.json", Context.MODE_PRIVATE).use {
+            it.write(response.toByteArray())
         }
+        parse(context)
+    } catch(e: Exception) {
+        // Hier können Sie handhaben, wie Sie auf den Fehler reagieren möchten.
+        // z.B. können Sie die Ausnahme ausdrucken, um zu sehen, was schief gelaufen ist:
+        e.printStackTrace()
+    } finally {
+        // close HTTP-Client
+        // Wir tun dies im finally-Block, um sicherzustellen, dass der Client auch im Falle eines Fehlers geschlossen wird.
+        client.close()
+    }
 }
-*/
+
+suspend fun beRequestLocation(context: Context, latitude: Double, longitude: Double) {
+    val client = HttpClient {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    val url = "http://10.0.2.2:8080/getCityByCoordinates?latitude=$latitude&longitude=$longitude"
+
+    try {
+        val response: String = client.get(url)
+
+        // Writes response to json file and fills dataclass
+        context.openFileOutput("filtered_shows.json", Context.MODE_PRIVATE).use {
+            it.write(response.toByteArray())
+        }
+        parse(context)
+    } catch(e: Exception) {
+        // Hier können Sie handhaben, wie Sie auf den Fehler reagieren möchten.
+        // z.B. können Sie die Ausnahme ausdrucken, um zu sehen, was schief gelaufen ist:
+        e.printStackTrace()
+    } finally {
+        // close HTTP-Client
+        // Wir tun dies im finally-Block, um sicherzustellen, dass der Client auch im Falle eines Fehlers geschlossen wird.
+        client.close()
+    }
+}
