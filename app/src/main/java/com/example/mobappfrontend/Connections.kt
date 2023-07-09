c1bc1b4fe922b3cab410c24803b3593aa67bfb0d
JulianDanzer <juliandanzer@outlook.com>
2023-07-09 12:00:27 +0200

    Final changes

--- app/src/main/java/com/example/mobappfrontend/Connections.kt
+++ app/src/main/java/com/example/mobappfrontend/Connections.kt
@@ -1,65 +1,59 @@
 package com.example.mobappfrontend
 
-import android.content.ClipData
 import android.content.Context
-import android.widget.Toast
 import io.ktor.client.HttpClient
-import io.ktor.client.features.ClientRequestException
-import io.ktor.client.features.RedirectResponseException
-import io.ktor.client.features.ServerResponseException
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.Json
 
 //TODO: Backend request
 //Class Connections.kt done by Julian
+
+//dbitems is the city name and context will be needed to access data operation
 suspend fun beRequestMan(context: Context, dbitem: String) {
     // Creation of HTTP-Client
-    val client = HttpClient {
-        install(JsonFeature) { }
-    }
+    val client = HttpClient {}
 
-    // Sending GET-Request to Backend-Server
     val url = "http://10.0.2.2:8080/$dbitem"
     try {
+        //send request to backend
         val response: String = client.get(url)
 
-        // Writes response to json file and fills dataclass
+        // opens "filtered_shows.json" from internal memory -> private is just for this application
         context.openFileOutput("filtered_shows.json", Context.MODE_PRIVATE).use {
             it.write(response.toByteArray())
         }
+        //calls parse function to store data in our list
         parse(context)
     } catch(e: Exception) {
         // Prints error if request was not success
         e.printStackTrace()
     } finally {
-        // close HTTP-Client
+        // close HTTP-Client whether its success or failed
         client.close()
     }
 }
 
+//context will be needed to access data operation, latitude and longitude are coordinates for request
 suspend fun beRequestLocation(context: Context, latitude: Double?, longitude: Double?) {
-    val client = HttpClient {
-        install(JsonFeature) { }
-    }
+    // Creation of HTTP-Client
+    val client = HttpClient {}
 
     val url = "http://10.0.2.2:8080/getCityByCoordinates?latitude=$latitude&longitude=$longitude"
 
     try {
+        //send request to backend
         val response: String = client.get(url)
 
-        // Writes response to json file and fills dataclass
+        // opens "filtered_shows.json" from internal memory -> private is just for this application
         context.openFileOutput("filtered_shows.json", Context.MODE_PRIVATE).use {
             it.write(response.toByteArray())
         }
+        //calls parse function to store data in our list
         parse(context)
     } catch(e: Exception) {
         // Prints error if request was not success
         e.printStackTrace()
     } finally {
-        // close HTTP-Client
+        // close HTTP-Client whether its success or failed
         client.close()
     }
 }
